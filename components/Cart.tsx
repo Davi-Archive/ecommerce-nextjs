8f06cdb1fbf8d4d8421422694e867ebe383f5229
davi38 <davi4alves@gmail.com>
2022-09-04 15:38:03 -0300

    useRef

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useRef, RefObject } from 'react'
+import Link from 'next/link';
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping
+} from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+
 
 
 
 const Cart = () => {
+  const cartRef = useRef() as RefObject<HTMLDivElement>;
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <button
+        type='button'
+        className='cart-heading'
+        onClick={() => setShowCart(false)}
+      >
+        <AiOutlineLeft />
+        <span>Seu Carrinho</span>
+        <span>({totalQuantities} items)</span>
+      </button>
+    </div>
   )
 }
 
